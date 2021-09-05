2b8b7c2e40d83f2f0b6b12c573fd0307f6928cf5
ermiry <erick.salas@ermiry.com>
2021-09-04 23:47:17 -0500

    Updated main app routes configuration

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import { BrowserRouter as  Router, Route, Switch, Redirect }  from 'react-router-dom';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// redux
+import { Provider } from 'react-redux';
+import store from './store';
+
+import Landing from './components/Landing';
+import Authentication from './components/Authentication';
+import NotFound from './components/NotFound';
+
+import PrivateRoute from './router/PrivateRoute';
+
+import { user_token_check } from './actions/authActions';
+
+user_token_check (store);
+
+class App extends Component {
+
+	render () {
+		return (
+			<Provider store= { store }>
+				<Router>
+					<div className="App">
+						<Switch>
+            	<Route exact path='/' component={ Landing } />
+
+							
+							
+							<Route exact path='/auth' component={ Authentication } />
+
+							{/* Other */}
+							<Route exact path='/404' component={ NotFound } />
+							<Redirect to="/404" />
+						</Switch>
+					</div>
+				</Router>
+			</Provider>
+		);
+	}
+
 }
 
 export default App;
